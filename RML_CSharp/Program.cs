e1e673cb8260d457b3cb34c46d5fc69bd5da019c
natlose <natlose@gmail.com>
2019-05-13 13:30:25 +0200

    generic examples

--- RML_CSharp/Program.cs
+++ RML_CSharp/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RML_CSharp
 {
+    class RaktariCikk { }
+    class Szolgaltatas { }
+
+    //Irhatsz saját generic típust, de valószínűbb hogy a System.Collections.Generic névtér osztályait fogod használni: 
+    // https://docs.microsoft.com/en-gb/dotnet/api/system.collections.generic?view=netframework-4.7.2
+    class SpeciLista<T> 
+    {
+        private T[] array = new T[10];
+        private int count;
+        
+        public void Add(T item)
+        {
+            if (count<array.Length)
+            {
+                array[count] = item;
+                count++;
+            }
+        }
+
+        public double GetSquareOfCount()
+        {
+            double c = count;
+            return c * c;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<RaktariCikk> cikkek = new List<RaktariCikk>();
+            List<Szolgaltatas> szolgaltatasok = new List<Szolgaltatas>();
+            cikkek.Add(new RaktariCikk());
+            cikkek.Add(new RaktariCikk());
+            szolgaltatasok.Add(new Szolgaltatas());
+            szolgaltatasok.Add(new Szolgaltatas());
+            foreach (var cikk in cikkek) Console.WriteLine(cikk.ToString());
+            foreach (var szolgaltatas in szolgaltatasok) Console.WriteLine(szolgaltatas.ToString());
+
+            SpeciLista<RaktariCikk> speciCikkek = new SpeciLista<RaktariCikk>();
+            speciCikkek.Add(new RaktariCikk());
+            speciCikkek.Add(new RaktariCikk());
+            Console.WriteLine(speciCikkek.GetSquareOfCount());
+
+            SpeciLista<Szolgaltatas> speciSzolgaltatasok = new SpeciLista<Szolgaltatas>();
+            speciSzolgaltatasok.Add(new Szolgaltatas());
+            speciSzolgaltatasok.Add(new Szolgaltatas());
+            speciSzolgaltatasok.Add(new Szolgaltatas());
+            Console.WriteLine(speciSzolgaltatasok.GetSquareOfCount());
+
         }
     }
 }
+
+// List<T> reference: https://docs.microsoft.com/en-gb/dotnet/api/system.collections.generic.list-1?view=netframework-4.7.2#methods
